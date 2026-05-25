d171c8f44a56301b9b6460539d82f0e29bfb84e0
Abhijeet Kasera <146567228+ABHIJEET-0001@users.noreply.github.com>
2026-05-25 22:19:23 +0530

    Time: 13 ms (12.28%) | Memory: 19.4 MB (53.85%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& state) {
+        state[node] = 1; // visiting
+
+        for (int nei : adj[node]) {
+            if (state[nei] == 1) return false;   // cycle found
+            if (state[nei] == 0 && !dfs(nei, adj, state)) return false;
+        }
+
+        state[node] = 2; // fully processed
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        for (auto &p : prerequisites) {
+            int a = p[0];
+            int b = p[1];
+            adj[b].push_back(a);   // b -> a
+        }
+
+        vector<int> state(numCourses, 0);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (!dfs(i, adj, state)) return false;
+            }
+        }
+
+        return true;
+    }
+};
