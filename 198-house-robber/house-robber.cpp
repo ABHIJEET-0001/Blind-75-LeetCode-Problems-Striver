2916dd7e92479089e346cdc56ce795d7f79c6fa7
Abhijeet Kasera <146567228+ABHIJEET-0001@users.noreply.github.com>
2026-06-01 23:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (76.18%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2 = 0; // i-2
+        int prev1 = 0; // i-1
+
+        for (int x : nums) {
+            int cur = max(prev1, prev2 + x);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+};
