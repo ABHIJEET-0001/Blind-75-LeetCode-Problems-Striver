3cf5e3e989fbf050928f6f3880ee8c6c3a6bf0d5
Abhijeet Kasera <146567228+ABHIJEET-0001@users.noreply.github.com>
2026-06-23 23:19:39 +0530

    Time: 27 ms (36.45%) | Memory: 23.9 MB (64.99%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+
+        int n = s.size();
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+
+        unordered_map<string, int> target;
+
+        for (string &w : words)
+            target[w]++;
+
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int count = 0;
+
+            unordered_map<string, int> window;
+
+            for (int right = offset; right + wordLen <= n; right += wordLen) {
+
+                string word = s.substr(right, wordLen);
+
+                if (target.count(word)) {
+
+                    window[word]++;
+                    count++;
+
+                    while (window[word] > target[word]) {
+                        string leftWord = s.substr(left, wordLen);
+
+                        window[leftWord]--;
+                        count--;
+                        left += wordLen;
+                    }
+
+                    if (count == wordCount) {
+                        ans.push_back(left);
+
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        count--;
+                        left += wordLen;
+                    }
+                }
+                else {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
